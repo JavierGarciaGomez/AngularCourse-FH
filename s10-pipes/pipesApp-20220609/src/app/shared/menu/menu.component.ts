7cdffb1cedab24d97266adb3a2ee76349b842cc1
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-06-10 10:27:33 +0200

    section 10-pipes

---
+++ s10-pipes/pipesApp-20220609/src/app/shared/menu/menu.component.ts
@@ -0,0 +1,42 @@
+import { MenuItem } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [],
+})
+export class MenuComponent implements OnInit {
+  constructor() {}
+  items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numeros',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'nocomunes',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+      },
+    ];
+  }
+}
