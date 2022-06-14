1abeef0d091b2bf8f2d43eff9aa14836804707b6
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-06-14 10:32:59 +0200

    section 20 lifecycle hooks

---
+++ s20-lifecyclehooks/lifecycle-app-20220614/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [],
+})
+export class MuestraNombreComponent implements OnInit {
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+}
