af3a6695a20988230f1deb5d2dc956c1ecf36b20
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-07-18 10:14:34 +0200

    s11-customPipes

---
+++ s11-customPipes/pipesApp20220718/src/app/ventas/pages/numeros/numeros.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numeros',
+  templateUrl: './numeros.component.html',
+  styles: [],
+})
+export class NumerosComponent implements OnInit {
+  constructor() {}
+  ventasNetas: number = 2567789.5567;
+  porcentaje: number = 0.4856;
+
+  ngOnInit(): void {}
+}
