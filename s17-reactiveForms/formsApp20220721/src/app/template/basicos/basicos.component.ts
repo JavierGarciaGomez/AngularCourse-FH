079b42e26d45f9dcb93f9dfd317d6c43643a5188
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-07-21 22:28:34 +0200

    Section 17 - reactive forms

---
+++ s17-reactiveForms/formsApp20220721/src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [],
+})
+export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+  initForm = {
+    producto: 'RTX 4080ti',
+    precio: 10,
+    existencias: 10,
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // guardar(miFormulario: NgForm) {
+  guardar() {
+    console.log('GUARDADO', this.miFormulario.value);
+    console.log(this.miFormulario);
+    this.miFormulario.resetForm(this.initForm);
+  }
+
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls.producto?.invalid &&
+      this.miFormulario?.controls.producto?.touched
+    );
+  }
+
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls.precio?.touched &&
+      this.miFormulario?.controls.precio?.value < 0
+    );
+  }
+}
